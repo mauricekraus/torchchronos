26cf1e78c088b4a3ba5b3c63fe10b32f71b53d5d
Maurice Kraus <dev@mkraus.io>
2023-03-24 16:59:45 +0100

    Cleanup

--- torchchronos/download.py
+++ torchchronos/download.py
@@ -1,31 +1,20 @@
-from collections.abc import Sequence
-import os
 from pathlib import Path
 from typing import Optional
-import warnings
 import zipfile
 
-import numpy as np
-import pandas as pd
-from sktime.datasets import load_UCR_UEA_dataset
 from sktime.datasets._data_io import _download_and_extract, _list_available_datasets
 
 
-
-
-def _download_uea_ucr(extract_path: Optional[Path], dataset_name: str):
+def download_uea_ucr(extract_path: Optional[Path], dataset_name: str):
     """This downloads the uea ucr dataset from sktime to the path extract_path."""
     # Download UCR/UEA archive from sktime
     if extract_path is not None:
-        
         local_dirname = extract_path
     else:
-        
         local_dirname = Path(".cache/data")
     local_dirname.mkdir(parents=True, exist_ok=True)
 
     if dataset_name not in _list_available_datasets(local_dirname):
-        
         # Dataset is not already present in the datasets directory provided.
         # If it is not there, download and install it.
         url = "https://timeseriesclassification.com/Downloads/%s.zip" % dataset_name
@@ -42,4 +31,3 @@ def _download_uea_ucr(extract_path: Optional[Path], dataset_name: str):
                 f"{extract_path}. Nor is it available on "
                 f"https://timeseriesclassification.com/.",
             ) from e
-
