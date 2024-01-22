fixed Normalization inverse

torchchronos/transforms/base.py
@@ -7,36 +7,56 @@ from aeon.transformations.base import BaseTransformer
 # TODO: implement scale and shift Transform, crop Transform, Dtype Transform, Reshape Transform
 
 class Transform(ABC):
-    def __init__(self):
-        self.is_fitted = False
-
-    def __call__(self, ts, targets=None):
-        self.transform(ts, targets)
+    def __init__(self, is_fitted: bool = False):
+        self.is_fitted = is_fitted
+        self._invert_transform = None
 
     def __add__(self, other):
         return Compose([self, other])
+    
+    def __invert__(self):
+        if self._invert_transform is None:
+            self._invert_transform = self._invert()
+            self._invert_transform._invert_transform = self
+        return self._invert_transform
 
     def fit_transform(self, ts, targets=None):
         self.fit(ts, targets)
         ts, target = self.transform(ts, targets)
         return ts, target
 
+    def fit(self, time_series, targets=None) -> None:
+        if self.is_fitted:
+            return
+        self._fit(time_series, targets)
+        self.is_fitted = True
+
+    def transform(self, time_series, targets=None) -> tuple[np.ndarray, np.ndarray | None]:
+        if not self.is_fitted:
+            raise Exception("Transform must be fitted before it can be used.")
+        return self._transform(time_series, targets)
+
+    @abstractmethod
+    def _fit(self, time_series, targets=None) -> None:
+        pass
+
+    @abstractmethod
+    def _transform(self, time_series, targets=None) -> tuple[np.ndarray, np.ndarray | None]:
+        pass
+
+    @abstractmethod
+    def _invert(self):
+        pass
+
     @abstractmethod
     def __repr__(self) -> str:
         pass
 
-    @abstractmethod
-    def fit(self, time_series, targets=None) -> None:
-        
-        pass
-
-    @abstractmethod
-    def transform(self, time_series, targets=None):
-        pass
 
 
 class Compose(Transform):
-    def __init__(self, transforms: list[BaseTransformer | Transform]):
+    def __init__(self, transforms: list[BaseTransformer | Transform], is_fitted=False):
+        super().__init__(is_fitted=is_fitted)
         self.transforms = transforms
         for t in self.transforms:
             if not isinstance(t, Transform):
@@ -48,8 +68,7 @@ class Compose(Transform):
                 ), "All transforms must have a transform method."
 
     def __add__(self, other):
-        self.transforms.append(other)
-        new_compose = Compose(self.transforms.append(other))
+        new_compose = Compose([*self.transforms, other])
         return new_compose
 
     def fit_transform(self, ts, targets=None):
@@ -58,17 +77,23 @@ class Compose(Transform):
 
         return ts, targets
 
-    def fit(self, ts, targets=None) -> None: # TODO: check if empty -> raise Exception?
+    def _fit(self, ts, targets=None) -> None:
+        if self.transforms == []:
+            return ts, targets
+        
         for t in self.transforms[:-1]:
             ts, targets = t.fit_transform(ts, targets)
 
         self.transforms[-1].fit(ts, targets)
         self.is_fitted = True
 
-    def transform(self, ts, targets=None) -> tuple[np.ndarray, np.ndarray]:
+    def _transform(self, ts, targets=None) -> tuple[np.ndarray, np.ndarray]:
         for t in self.transforms:
             ts, targets = t.transform(ts, targets)
         return ts, targets
+    
+    def _invert(self) -> Transform:
+        return Compose([~t for t in self.transforms[::-1]])
 
     def __repr__(self):
         format_string = self.__class__.__name__ + "("

torchchronos/transforms/transforms.py
@@ -1,52 +1,48 @@
 import numpy as np
 import torch
-from .base import Transform
+from .base import Transform, Compose
 
 
 """
 This class transforms the labels of a dataset. It transforms arbitrary string/int labels to int labels form 0 to n_classes-1.
 """
 class Identity(Transform):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, is_fitted = True) -> None:
+        super().__init__(is_fitted=is_fitted)
 
-    def fit(self, time_series: np.ndarray, y = None) -> None:
+    def _fit(self, time_series: np.ndarray, y = None) -> None:
         pass
 
-    def transform(self, time_series: np.ndarray, y = None) -> np.ndarray:
+    def _transform(self, time_series: np.ndarray, y = None) -> np.ndarray:
         return time_series, y
+    
+    def _invert(self) -> Transform:
+        return self
 
     def __repr__(self) -> str:
         return "Identity()"
     
 class LabelTransform(Transform):
-    def __init__(self) -> None:
-        super().__init__()
-        self.label_map = None
+    def __init__(self, label_map = None) -> None:
+        super().__init__(True if label_map is None else False)
+        self.label_map = label_map
 
     def __repr__(self) -> str:
         return f"TransformLabels(label_map={self.label_map})"
 
-    def fit(self, time_series: np.ndarray, y) -> None:
+    def _fit(self, time_series: np.ndarray, y) -> None:
         labels = np.unique(y)
         labels = np.sort(labels)
         self.label_map = {i: label for label, i in enumerate(labels)}
 
-    def transform(self, time_series: np.ndarray, y) -> tuple[np.ndarray, np.ndarray]:
+    def _transform(self, time_series: np.ndarray, y) -> tuple[np.ndarray, np.ndarray]:
         new_labels = np.array([self.label_map[label] for label in y])
         return time_series, new_labels
     
-class LocalNormalize(Transform):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def fit(self, time_series: np.ndarray, y = None) -> Transform:
-        pass
-
-    def transform(self, time_series: np.ndarray, y = None) -> np.ndarray:
-        mean = np.mean(time_series, axis = 1)
-        std = np.std(time_series, axis = 1)
-        return (time_series - mean[:, None]) / std[:, None]
+    def _invert(self) -> Transform:
+        label_map = {v: k for k, v in self.label_map.items()}
+        return LabelTransform(label_map)
+    
 
 class GlobalNormalize(Transform):
     def __init__(self) -> None:
@@ -54,13 +50,54 @@ class GlobalNormalize(Transform):
         self.mean = None
         self.std = None
 
-    def fit(self, time_series: torch.Tensor, y = None) -> None:
+    def _fit(self, time_series: torch.Tensor, y = None) -> None:
         self.mean = torch.mean(time_series, axis = 0)
         self.std = torch.std(time_series, axis = 0)
 
-    def transform(self, time_series: torch.Tensor, y = None) -> tuple[np.ndarray, np.ndarray]:
+    def _transform(self, time_series: torch.Tensor, y = None) -> tuple[np.ndarray, np.ndarray]:
         return (time_series - self.mean) / self.std, y
 
     def __repr__(self) -> str:
         return f"Normalize(mean={self.mean}, std={self.std})"
+    
+    def _invert(self) -> Transform:
+        if self.mean is None or self.std is None:
+            raise RuntimeError("Cannot invert transform before fitting.")
+        
+        return Compose([Scale(self.std), Shift(self.mean)], is_fitted=True)
+    
+
+class Scale(Transform):
+    def __init__(self, scale: float) -> None:
+        super().__init__(True)
+        self.scale: torch.Tensor | float = scale
+
+    def _fit(self, time_series, targets=None) -> None:
+        pass
+
+    def _transform(self, time_series: torch.Tensor, y = None) -> tuple[np.ndarray, np.ndarray | None]:
+        return time_series * self.scale, y
+    
+    def _invert(self) -> Transform:
+        return Scale(1/self.scale)
+    
+    def __repr__(self) -> str:
+        return f"Scale(scale={self.scale})"
+
+class Shift(Transform):
+    def __init__(self, shift: int) -> None:
+        super().__init__(True)
+        self.shift = shift
+
+    def _fit(self, time_series, targets=None) -> None:
+        pass
+
+    def _transform(self, time_series: torch.Tensor, y = None) -> tuple[np.ndarray, np.ndarray]:
+        return time_series + self.shift , y
+
+    def __repr__(self) -> str:
+        return f"Shift(shift={self.shift})"
+    
+    def _invert(self) -> Transform:
+        return Shift(-self.shift)
 