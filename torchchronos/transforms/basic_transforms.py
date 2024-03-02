ffaaf990862a86a63c9a7e45466fb7056e4504b6
PaulRabich <paul.j.rabich@gmail.com>
2024-03-02 19:56:22 +0100

    formatting

--- torchchronos/transforms/basic_transforms.py
+++ torchchronos/transforms/basic_transforms.py
@@ -9,13 +9,11 @@ class Identity(Transform):
     def __init__(self, is_fitted=True) -> None:
         super().__init__(is_fitted=is_fitted)
 
-    def _fit(
-        self, time_series:torch.Tensor, targets:Optional[torch.Tensor]= None
-        ) -> None:
+    def _fit(self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None) -> None:
         pass
 
     def _transform(
-        self, time_series: torch.Tensor, targets:Optional[torch.Tensor] = None
+        self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None
     ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         return time_series, targets
 
@@ -27,22 +25,20 @@ class Identity(Transform):
 
 
 class Normalize(Transform):
-    def __init__(self, local:bool= False) -> None:
+    def __init__(self, local: bool = False) -> None:
         super().__init__(local)
         self.local = local
-        self.mean:Optional[torch.Tensor] = None
-        self.std:Optional[torch.Tensor] = None
+        self.mean: Optional[torch.Tensor] = None
+        self.std: Optional[torch.Tensor] = None
 
-    def _fit(
-        self, time_series:torch.Tensor, targets:Optional[torch.Tensor]= None
-        ) -> None:
+    def _fit(self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None) -> None:
         if self.local:
             return
         self.mean = torch.mean(time_series, 0, True)
         self.std = torch.std(time_series, 0, True) + 1e-5
 
     def _transform(
-        self, time_series: torch.Tensor, targets:Optional[torch.Tensor] = None
+        self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None
     ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         if self.local:
             mean = torch.mean(time_series, 2, True)
@@ -70,7 +66,7 @@ class Normalize(Transform):
         if self.mean is None or self.std is None:
             raise RuntimeError("Cannot invert transform before fitting.")
 
-        return Compose([Scale(self.std), Shift(self.mean)], is_fitted=True)
+        return Compose([Scale(self.std), Shift(self.mean)])
 
 
 class Scale(Transform):
@@ -78,13 +74,11 @@ class Scale(Transform):
         super().__init__(True)
         self.scale: float | torch.Tensor = scale
 
-    def _fit(
-        self, time_series:torch.Tensor, targets:Optional[torch.Tensor]= None
-        ) -> None:
+    def _fit(self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None) -> None:
         pass
 
     def _transform(
-        self, time_series: torch.Tensor, targets:Optional[torch.Tensor] = None
+        self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None
     ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         return time_series * self.scale, targets
 
@@ -95,7 +89,7 @@ class Scale(Transform):
         if isinstance(self.scale, torch.Tensor):
             return f"Scale({self.scale.shape})"
         else:
-            #single value
+            # single value
             return f"Scale({self.scale})"
 
 
@@ -104,13 +98,11 @@ class Shift(Transform):
         super().__init__(True)
         self.shift: float | torch.Tensor = shift
 
-    def _fit(
-        self, time_series:torch.Tensor, targets:Optional[torch.Tensor]= None
-        ) -> None:
+    def _fit(self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None) -> None:
         pass
 
     def _transform(
-        self, time_series: torch.Tensor, targets:Optional[torch.Tensor] = None
+        self, time_series: torch.Tensor, targets: Optional[torch.Tensor] = None
     ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         return time_series + self.shift, targets
 
@@ -118,7 +110,7 @@ class Shift(Transform):
         if isinstance(self.shift, torch.Tensor):
             return f"Shift(shift={self.shift.shape})"
         else:
-            #single value
+            # single value
             return f"Shift(shift={self.shift})"
 
     def _invert(self) -> Transform:
