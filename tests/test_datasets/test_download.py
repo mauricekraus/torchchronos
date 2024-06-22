b66da09e35f2747bde2c3ca1b73087308639f88b
Paul Rabich <paul.j.rabich@gmail.com>
2024-06-22 16:03:53 +0200

    updated aeon

--- tests/test_datasets/test_download.py
+++ tests/test_datasets/test_download.py
@@ -1,18 +1,18 @@
 import pytest
-from aeon.datasets._data_loaders import load_forecasting, load_regression
-from aeon.datasets.tser_data_lists import tser_all
-from aeon.datasets.tsf_data_lists import tsf_all
+from aeon.datasets._data_loaders import load_forecasting, load_regression, load_classification
+from aeon.datasets.tser_datasets import tser_monash
+from aeon.datasets.tsc_datasets import univariate, univariate2015, multivariate
 
 
 @pytest.mark.download_all
-@pytest.mark.parametrize("dataset_name", tsf_all)
-def test_download_all_tsf(dataset_name, tmp_path):
-    data = load_forecasting(dataset_name, tmp_path)
+@pytest.mark.parametrize("dataset_name", univariate)
+def test_download_all_tsc(dataset_name, tmp_path):
+    data = load_classification(name=dataset_name, extract_path=tmp_path)
     len(data)
 
 
 @pytest.mark.download_all
-@pytest.mark.parametrize("dataset_name", tser_all)
+@pytest.mark.parametrize("dataset_name", tser_monash)
 def test_download_all_tser(dataset_name, tmp_path):
     data = load_regression(name=dataset_name, extract_path=tmp_path)
     len(data)
